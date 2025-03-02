bc636fb62d59226dec5f1bfc77b2e9698c7cc972
Nathnael <nathnaelyirga@gmail.com>
2025-03-02 17:09:36 +0300

    seo optimization v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="">
+      <head>
+        <meta name="title" content="Nathnael Yirga" />
+        <meta name="description" content="Personal Portfolio - Nathnael Yirga" />
+        <meta name="author" content="Nathnael Yirga" />
+        <meta name="keywords" content="portfolio, developer, Nathnael Yirga" />
+        <meta property="og:title" content="Nathnael Yirga - Portfolio" />
+        <meta property="og:description" content="Welcome to my portfolio website!" />
+        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nathnaelyirga.dev" />
+      </head>
       <body className="bg-gradient-to-r from-red-900 to-slate-700">
         {children}
       </body>
