9e37e3a5c8ed17c02b1552c0639ab31b93f9c68f
Nathnael Yirga <Nathnaelyirga@gmail.com>
2025-05-22 20:55:46 +0300

    chat app in projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,10 +1,12 @@
-import projectsData from "../../public/assets/projects_data";
+import projectsData from "@/public/assets/projects_data";
 import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
     <div className="w-full">
-      <p className="text-start mx-10 my-10 font-extrabold text-5xl text-white">Projects</p>
+      <p className="text-start mx-10 my-10 font-extrabold text-5xl text-white">
+        Projects
+      </p>
       <div className="flex flex-wrap m-5 text-center w-full gap-7">
         {projectsData.map((project, index) => (
           <ProjectCard key={index} {...project} />
--- public/assets/projects_data.js
+++ public/assets/projects_data.js
@@ -1,50 +1,58 @@
 const projectsData = [
-    {
-      title: "CampusSync - API",
-      description:
-        "Hackathon project built with Django REST Framework, DRF-spectacular for schema generation, JWT for authentication ...",
-      link: "https://natty.pythonanywhere.com",
-      linkText: "Visit Site",
-      color: "#E84023",
-      iconType: "language",
-    },
-    {
-      title: "YouTube-like App",
-      description:
-        "YouTube-like app using Flask, integrating SQLAlchemy for database operations, WTForms for form handling, and Jinja2 for templating.",
-      link: "https://github.com/Nxttyy/YouTube-Flask",
-      linkText: "GitHub",
-      color: "#E84023",
-      iconType: "github",
-    },
-    {
-      title: "Library-genesis",
-      description:
-        "CLI tool built with Python for downloading books from Library Genesis. It allows users to search for books by title, author, or ISBN and download them directly.",
-      link: "https://github.com/Nxttyy/libgen-api-cli",
-      linkText: "GitHub",
-      color: "#E84023",
-      iconType: "github",
-    },
-    {
-      title: "Tele-Mart",
-      description:
-        "A marketplace Telegram bot built using Python and the python-telegram-bot library. Features include creating, listing, and purchasing items, with Chapa payment integration.",
-      link: "https://github.com/Nxttyy/Tele-Mart",
-      linkText: "GitHub",
-      color: "#E84023",
-      iconType: "github",
-    },
-    {
-      title: "English to Amharic Dictionary",
-      description:
-        "An open-source English to Amharic dictionary that allows collaborative translation and updates between the two languages.",
-      link: "https://github.com/Nxttyy/AmharicDictionary",
-      linkText: "GitHub",
-      color: "#E84023",
-      iconType: "github",
-    },
-  ];
-  
-  export default projectsData;
-  
+  {
+    title: "CampusSync - API",
+    description:
+      "Hackathon project built with Django REST Framework, DRF-spectacular for schema generation, JWT for authentication ...",
+    link: "https://natty.pythonanywhere.com",
+    linkText: "Visit Site",
+    color: "#E84023",
+    iconType: "language",
+  },
+  {
+    title: "Chat Room",
+    description:
+      "Privacy first, no-signin chatting application with support for unlimitted attendees.",
+    link: "https://github.com/Nxttyy/Chat-App",
+    linkText: "GitHub",
+    color: "#E84023",
+    iconType: "github",
+  },
+  {
+    title: "YouTube-like App",
+    description:
+      "YouTube-like app using Flask, integrating SQLAlchemy for database operations, WTForms for form handling, and Jinja2 for templating.",
+    link: "https://github.com/Nxttyy/YouTube-Flask",
+    linkText: "GitHub",
+    color: "#E84023",
+    iconType: "github",
+  },
+  {
+    title: "Library-genesis",
+    description:
+      "CLI tool built with Python for downloading books from Library Genesis. It allows users to search for books by title, author, or ISBN and download them directly.",
+    link: "https://github.com/Nxttyy/libgen-api-cli",
+    linkText: "GitHub",
+    color: "#E84023",
+    iconType: "github",
+  },
+  {
+    title: "Tele-Mart",
+    description:
+      "A marketplace Telegram bot built using Python and the python-telegram-bot library. Features include creating, listing, and purchasing items, with Chapa payment integration.",
+    link: "https://github.com/Nxttyy/Tele-Mart",
+    linkText: "GitHub",
+    color: "#E84023",
+    iconType: "github",
+  },
+  {
+    title: "English to Amharic Dictionary",
+    description:
+      "An open-source English to Amharic dictionary that allows collaborative translation and updates between the two languages.",
+    link: "https://github.com/Nxttyy/AmharicDictionary",
+    linkText: "GitHub",
+    color: "#E84023",
+    iconType: "github",
+  },
+];
+
+export default projectsData;
